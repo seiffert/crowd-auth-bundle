04ae4850f4ece5460e073d82fa2f885408b0cc80
Paul Seiffert <paul.seiffert@gmail.com>
2013-03-23 16:34:01 +0100

    Added travis support and phpunit infrastructure

---
+++ Tests/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+if (!is_file($autoloadFile = __DIR__.'/../vendor/autoload.php')) {
+    throw new \LogicException('Could not find vendor/autoload.php. Did you run "composer install --dev"?');
+}
+
+require_once $autoloadFile;
+
+\Doctrine\Common\Annotations\AnnotationRegistry::registerLoader('class_exists');
